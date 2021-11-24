b5fac17559f01d49f35ad388c67d5f5fd3265314
Chris <cdesch@gmail.com>
2021-11-24 13:38:08 -0500

    adding gtin dep

--- mix.exs
+++ mix.exs
@@ -48,7 +48,8 @@ defmodule ExGtinApi.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:ex_gtin, "~> 1.0.2"}
     ]
   end
 
